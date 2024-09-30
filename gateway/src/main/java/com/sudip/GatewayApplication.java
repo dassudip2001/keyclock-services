c706c4ff239de8501564624076ee0a120efad396
Sudip Das <sudip5428@gmail.com>
2024-09-30 18:41:24 +0530

    init

--- gateway/src/main/java/com/sudip/GatewayApplication.java
+++ gateway/src/main/java/com/sudip/GatewayApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class SwiggyGatewayApplication {
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SwiggyGatewayApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
